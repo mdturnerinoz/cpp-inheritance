cefd7f905e8680e8be0c3085f95fd4d245448a0a
Marty Turner <mdturnerinoz@gmail.com>
2013-12-03 11:08:45 +1100

    comment example2

--- example2.cpp
+++ example2.cpp
@@ -1,3 +1,32 @@
+/*
+
+So we see that the public members of class ‘fourwheeler’ were easily accessed as public members 
+of class ‘car’. Similarly we can do protected and private inheritance using protected and 
+private access specifiers.
+
+One important thing to understand here is how to decide whether public or protected or private 
+inheritance is required? Here is the answer :
+
+* When the inheritance is public, the derived class and every body else can access the public 
+  members of base class through derived class because these members now become the public 
+  members of derived class. So, it for you to decide if you want this kind of visibility or not?
+
+* When the inheritance is protected, only the derived class and it’s children can access these 
+  members as these now become the protected members of derived class. Other than them, no one 
+  else can access them directly. So, again, its for you to decide if you want this kind of 
+  visibility.
+
+* Similarly, when the inheritance is private, only and only the derived class can access these 
+  members as these now become the private members of derived class and hence cannot be derived 
+  further. Also, no one else can use them ever. So, here again, it’s for you to decide when to opt 
+  for private inheritance.
+
+Moving ahead, the example we just discussed earlier represents single inheritance. There can 
+be multiple inheritance too. This means that a single class can inherit from more than one 
+class simultaneously. Here is an example of that :
+
+*/
+
 #include<iostream>
 
 class sedans
